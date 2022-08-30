f4f654192fbeeb7b54c7e3380525a71f34956fbf
Andrik61 <andrey@mail.ru>
2022-08-30 13:16:26 +0300

    Lekcia

---
+++ Seminar1/Program.cs
@@ -0,0 +1,82 @@
+﻿// Lekcia
+// int numberA = 3;
+// int numberB = 5;
+// int result = numberA + numberB;
+// Console.WriteLine(result);
+
+// double numberA = 12;
+// double numberB = 5;
+// System.Console.WriteLine(numberA / numberB);
+
+// int numberA = new Random().Next(1, 10);
+// System.Console.WriteLine(numberA);
+// int numberB = new Random().Next(1, 10);
+// Console.WriteLine(numberB);
+// int result = numberA + numberB;
+// Console.WriteLine(result);
+
+// Console.WriteLine("Введите имя пользователя: ");
+// string username = Console.ReadLine();
+// if (username.ToLower() == "маша")
+// {
+//     Console.WriteLine("Ура, это же Маша!");
+// }
+// else
+// {
+//     Console.Write("Привет, ");
+//     Console.Write(username);
+// }
+
+// int a = 1;
+// int b = 2;
+// int c = 6;
+// int d = 8;
+// int e = 4;
+// int max = a;
+// if (a > max ) max = a;
+// if (b > max ) max = b;
+// if (c > max ) max = c;
+// if (d > max ) max = d;
+// if (e > max ) max = e;
+// Console.Write("max = ");
+// Console.WriteLine(max);
+
+// Console.SetCursorPosition(8, 4);
+// Console.WriteLine("+");
+// int xa = 1, ya = 1,
+//     xb = 1, yb = 10,
+//     xc = 10, yc = 10;
+//  Console.SetCursorPosition(xa, ya);
+//  Console.WriteLine("+");
+//   Console.SetCursorPosition(xb, yb);
+//  Console.WriteLine("+");
+//   Console.SetCursorPosition(xc, yc);
+//  Console.WriteLine("+");
+// int x = xa, y = xb;
+// int count = 0;
+// while (count < 10)
+// {
+//     int what = new Random().Next(0, 3);
+//     if(what == 0)
+//     {
+//         x = (x + xa) / 2;
+//         y = (y + ya) / 2;
+//     }
+//     if(what == 1)
+//     {
+//         x = (x + xb) / 2;
+//         y = (y + yb) / 2;
+//     }
+//     if(what == 2)
+//     {
+//         x = (x + xc) / 2;
+//         y = (y + yc) / 2;
+//     }
+//     Console.SetCursorPosition(x, y);
+//     Console.WriteLine("+");
+//     count++;
+// }
+
+// Seminar
+
+
